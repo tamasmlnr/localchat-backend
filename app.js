1855fe2cec0c32f6e778622a60c655fa38685565
tamasmlnr <molnart90@gmail.com>
2025-03-06 16:28:51 +0100

    feat: add socket io connection and messages

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const { MONGO_URI } = require('./utils/config');
 const { requestLogger } = require('./utils/middleware');
+const http = require('http');
+const setupSocketIO = require('./services/socket');
 
 const mongoUrl = MONGO_URI;
 console.log(`connecting to ${mongoUrl}`);
@@ -16,6 +18,9 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true }).then(() => {
 }).catch((err) => {
     console.log("Error connecting to database! ", err);
 });
+
+const server = http.createServer(app);
+setupSocketIO(server);
 
 app.use(cors())
 app.use(express.json())
